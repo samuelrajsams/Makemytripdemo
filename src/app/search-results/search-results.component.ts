6660f906e0eb79e967ab0e6a0f6ea390ab0696ce
bhadri1255 <bhadri1255@gmail.com>
2024-02-11 16:45:03 +0530

    search results

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -7,5 +7,116 @@ import { MatList} from '@angular/material/list';
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent {
-  @Input() flights: any[] | undefined;
+  // @Input() flights: any[] | undefined;
+ flights: any =  [
+  {
+    "airline": "Delta Airlines",
+    "origin": "New York",
+    "destination": "Los Angeles",
+    "departure": "08:00 AM",
+    "arrival": "11:00 AM",
+    "duration": "3h",
+    "price": "$250"
+  },
+  {
+    "airline": "United Airlines",
+    "origin": "Chicago",
+    "destination": "Boston",
+    "departure": "09:30 AM",
+    "arrival": "12:30 PM",
+    "duration": "3h",
+    "price": "$280"
+  },
+  {
+    "airline": "American Airlines",
+    "origin": "Boston",
+    "destination": "Miami",
+    "departure": "11:00 AM",
+    "arrival": "02:00 PM",
+    "duration": "3h",
+    "price": "$300"
+  },
+  {
+    "airline": "Southwest",
+    "origin": "Denver",
+    "destination": "Las Vegas",
+    "departure": "01:00 PM",
+    "arrival": "03:00 PM",
+    "duration": "2h",
+    "price": "$200"
+   },
+   {
+    "airline": "Delta Airlines",
+    "origin": "New York",
+    "destination": "Los Angeles",
+    "departure": "08:00 AM",
+    "arrival": "11:00 AM",
+    "duration": "3h",
+    "price": "$250"
+  },
+  {
+    "airline": "United Airlines",
+    "origin": "Chicago",
+    "destination": "Boston",
+    "departure": "09:30 AM",
+    "arrival": "12:30 PM",
+    "duration": "3h",
+    "price": "$280"
+  },
+  {
+    "airline": "American Airlines",
+    "origin": "Boston",
+    "destination": "Miami",
+    "departure": "11:00 AM",
+    "arrival": "02:00 PM",
+    "duration": "3h",
+    "price": "$300"
+  },
+  {
+    "airline": "Southwest",
+    "origin": "Denver",
+    "destination": "Las Vegas",
+    "departure": "01:00 PM",
+    "arrival": "03:00 PM",
+    "duration": "2h",
+    "price": "$200"
+   },
+   {
+    "airline": "Delta Airlines",
+    "origin": "New York",
+    "destination": "Los Angeles",
+    "departure": "08:00 AM",
+    "arrival": "11:00 AM",
+    "duration": "3h",
+    "price": "$250"
+  },
+  {
+    "airline": "United Airlines",
+    "origin": "Chicago",
+    "destination": "Boston",
+    "departure": "09:30 AM",
+    "arrival": "12:30 PM",
+    "duration": "3h",
+    "price": "$280"
+  },
+  {
+    "airline": "American Airlines",
+    "origin": "Boston",
+    "destination": "Miami",
+    "departure": "11:00 AM",
+    "arrival": "02:00 PM",
+    "duration": "3h",
+    "price": "$300"
+  },
+  {
+    "airline": "Southwest",
+    "origin": "Denver",
+    "destination": "Las Vegas",
+    "departure": "01:00 PM",
+    "arrival": "03:00 PM",
+    "duration": "2h",
+    "price": "$200"
+  }
+]
+
 }
